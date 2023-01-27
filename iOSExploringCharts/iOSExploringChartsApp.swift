9bd19c90774cb71403600e486b30425cb540413a
VirajTank <virajtank98@gmail.com>
2023-01-27 18:30:03 +0530

    Initial Commit

---
+++ iOSExploringCharts/iOSExploringChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSExploringChartsApp.swift
+//  iOSExploringCharts
+//
+//  Created by Viraj Tank on 27/01/23.
+//
+
+import SwiftUI
+
+@main
+struct iOSExploringChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
